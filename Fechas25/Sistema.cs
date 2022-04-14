a1006a7bb96aa44e14edc103faf6120f6e6ac995
JoelCAI <joelargentina12345@gmail.com>
2022-04-13 23:22:02 -0300

    Agregar archivos de proyecto.

---
+++ Fechas25/Sistema.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fechas25
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            Validador.Bienvenida();
+            Console.WriteLine("\n Recuerde seguir las instrucciones. Presione cualquier tecla para continuar");
+
+            int numeroUno = Validador.PedirInt("\n Ingrese el primer número que será el año" +
+                                               "\n Utilize el formato *aaaa*");
+            Console.Clear();
+            int numeroDos = Validador.PedirInt("\n Ingrese el primer número que será el mes" +
+                                               "\n Utilize el formato *mm*");
+            Console.Clear();
+            int numeroTres = Validador.PedirInt("\n Ingrese el primer número que será el día" +
+                                                "\n Utilize el formato *dd*");
+            Console.Clear();
+            Validador.FechaConstructor(numeroUno, numeroDos, numeroTres);
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+
+
+    }
+}
---
+++ Fechas25/Validador.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Fechas25
+{
+    public class Validador
+    {
+        public static void Despedida()
+        {
+
+            Console.WriteLine("\n Gracias por usar nuestro Sistema presione cualquier teclar para finalizar");
+            Console.ReadKey();
+        }
+
+
+        public static int PedirInt(string mensaje)
+        {
+            /* la salida de esta funcion estará en la variable long valor */
+            string mensError = "\n\n Por favor ingrese el valor solicitado" +
+                               "\n Debe ingresar un valor que no sea vacio y que ese valor sea un numero. ";
+
+            int valor;
+            Console.WriteLine(mensaje);
+
+            if (!int.TryParse(Console.ReadLine(), out valor))
+            {
+                Console.Clear();
+                Console.WriteLine(mensError);
+                return PedirInt(mensaje);
+            }
+            else
+                return valor;
+        }
+
+        public static void FechaConstructor(int numeroUno, int numeroDos, int numeroTres)
+        {
+            string año = Convert.ToString(numeroUno);
+            string mes = Convert.ToString(numeroDos);
+            string dia = Convert.ToString(numeroTres);
+
+            string fecha = "" + numeroUno + "/" + numeroDos + "/" + numeroTres + "";
+
+            bool ingresoCorrecto;
+            DateTime fechaValida;
+
+            ingresoCorrecto = DateTime.TryParse(fecha, out fechaValida);
+
+            if (!ingresoCorrecto)
+            {
+                Console.Clear();
+                Console.WriteLine("\n Usted ingreso una fecha inválida");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("\n Usted ingreso una fecha correcta que es: *" + fechaValida + "*.");
+            }
+
+        }
+
+        public static void Bienvenida()
+        {
+            Console.WriteLine("\n Bienvenido al Programa");
+        }
+    }
+}
